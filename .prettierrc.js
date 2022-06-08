a92ec9716bf5389ef064b41265aea762d2760137
Marthijs Berfelo <marthijs.berfelo@hybrit.org>
2022-06-08 22:11:54 +0200

    add es-lint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "tabWidth": 2,
+  "singleQuote": true,
+  "trailingComma": "es5",
+  "useTabs": false,
+  "quoteProps": "consistent",
+  "printWidth": 100,
+  "endOfLine": "auto"
+};
