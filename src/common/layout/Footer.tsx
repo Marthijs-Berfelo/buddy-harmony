19989c9538b90142c9380a0ee24b3f952b70c7dd
Marthijs Berfelo <mwberfelo@gmail.com>
2023-02-27 09:47:53 +0100

    fix footer icon

--- src/common/layout/Footer.tsx
+++ src/common/layout/Footer.tsx
@@ -3,9 +3,10 @@ import { useTranslation } from 'react-i18next';
 import { Tooltip, Typography } from '@material-tailwind/react';
 import packageJson from '../../../package.json';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faTwitter, IconDefinition } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faAt, faBug } from '@fortawesome/free-solid-svg-icons';
 import { appInfo } from '../app-info';
+import { IconDefinition } from "@fortawesome/fontawesome-common-types";
 
 const Footer = (): JSX.Element => {
   const { t } = useTranslation();
