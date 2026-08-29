b13959dc021825c677f600be281d495b16cfe335
marthijs.berfelo <marthijs.berfelo@edsn.nl>
2026-08-29 19:09:58 +0200

    fix: replace Material Tailwind ThemeProvider with shadcn TooltipProvider
    - ThemeProvider bundled a private React 18 copy, causing a React version
      mismatch crash under this app's React 19 tree
    
    Refs: #556

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from '@material-tailwind/react';
+import { TooltipProvider } from './components/ui/tooltip';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <ThemeProvider value={{}}>
+    <TooltipProvider>
       <App />
-    </ThemeProvider>
+    </TooltipProvider>
   </React.StrictMode>
 );
 
